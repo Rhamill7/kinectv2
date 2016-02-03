76fc729d32a2fe643dac501790e6e2597a680f0c
Robbie Hamill <robbierabhamill@gmail.com>
2016-02-03 16:25:42 +0000

    Working on Bitmapping source to get spcific pixel location

--- BitMapTest/BitMapTest/Form1.cs
+++ BitMapTest/BitMapTest/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace BitMapTest
 {
@@ -19,11 +21,26 @@ namespace BitMapTest
 
         private void Bitmaptest_Load(object sender, EventArgs e)
         {
+
+          //  pictureBox1.Image = System.Drawing.Image.FromFile("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png");
             //read image
-            Bitmap bitmap = new Bitmap("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png");
+            BitmapSource bitmap = new BitmapImage((new Uri("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png")));
 
             //load
             pictureBox1.Image = bitmap;
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            BitmapSource bSource = new BitmapImage(new Uri("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png"));
+           // BitmapSource img = pictureBox1.Image.get
+
+           // int stride = pictureBox1.Image.PixelWidth * 4;
+            int size = img.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            img.CopyPixels(pixels, stride, 0);
+
         }
     }
 }
