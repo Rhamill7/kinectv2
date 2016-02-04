9fe234dee8537abb076f6adda6b8766858444fd1
Robbie Hamill <robbierabhamill@gmail.com>
2016-02-04 17:18:28 +0000

    Attempted unsuccessfully to color a pixel that was selected at a particular point.

--- BitMapTest/BitMapTest/Form1.cs
+++ BitMapTest/BitMapTest/Form1.cs
@@ -13,7 +13,11 @@ using System.Windows.Media.Imaging;
 namespace BitMapTest
 {
     public partial class Bitmaptest : Form
-    {
+    {   
+        //Global Variables for pixel data?
+        Color[,] pixelData = null;
+//        Point P = new Point (0,0);
+
         public Bitmaptest()
         {
             InitializeComponent();
@@ -21,26 +25,52 @@ namespace BitMapTest
 
         private void Bitmaptest_Load(object sender, EventArgs e)
         {
+            int x = 0; int y = 0;
 
-          //  pictureBox1.Image = System.Drawing.Image.FromFile("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png");
             //read image
-            BitmapSource bitmap = new BitmapImage((new Uri("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png")));
+            // Bitmap myBitmap = new Bitmap("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png");
+            Bitmap myBitmap = getBitmap();  
+            ///Get Data
+            //Color[,]
+                pixelData = new Color[myBitmap.Width, myBitmap.Height];
 
-            //load
-            pictureBox1.Image = bitmap;
+            for (y = 0; y < myBitmap.Height; y++)
+                for (x = 0; x < myBitmap.Width; x++)
+                    pixelData[x, y] = myBitmap.GetPixel(x, y);
+           
+            //loadimage onto Screen
+            pictureBox1.Image = myBitmap;
         }
 
-        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point P = new Point(e.X,e.Y);
+
+            for (int i = 0; i < pixelData.Length; i++) {
+                if (pixelData.GetValue(i).Equals(P))
+                {
+
+                    Bitmap myBitmap = getBitmap();
+                    myBitmap.SetPixel(e.X, e.Y, Color.Red);
+                    pictureBox1.Image = myBitmap;
+                }
+               }
+
+        //    BitmapSource bSource = new BitmapImage(new Uri("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png"));
+           // BitmapSource img = pictureBox1.Image.get
+        //    int stride = bSource.PixelWidth * 4;
+       //     int size = bSource.PixelHeight * stride;
+       //     byte[] pixels = new byte[size];
+        //    bSource.CopyPixels(pixels, stride, 0);
+
+        }
+
+        public Bitmap getBitmap()
         {
 
-            BitmapSource bSource = new BitmapImage(new Uri("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png"));
-           // BitmapSource img = pictureBox1.Image.get
-
-           // int stride = pictureBox1.Image.PixelWidth * 4;
-            int size = img.PixelHeight * stride;
-            byte[] pixels = new byte[size];
-            img.CopyPixels(pixels, stride, 0);
-
+            Bitmap myBitmap = new Bitmap("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png");
+            return myBitmap;
         }
+
     }
 }
