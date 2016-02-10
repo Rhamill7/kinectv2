5a5cd44c19a202ff56983418a9c808388ebf8366
Robbie Hamill <robbierabhamill@gmail.com>
2016-02-10 17:29:21 +0000

    Overcomplicated the methods. Removed code i didnt need and saved the pixel position instead of the colour which i didnt realise i was doing. It now works as intended.

--- BitMapTest/BitMapTest/Form1.cs
+++ BitMapTest/BitMapTest/Form1.cs
@@ -15,7 +15,9 @@ namespace BitMapTest
     public partial class Bitmaptest : Form
     {   
         //Global Variables for pixel data?
-        Color[,] pixelData = null;
+        //Color[,] pixelData = null;
+        List<Point> pixelPoint = new List<Point>();
+
 //        Point P = new Point (0,0);
 
         public Bitmaptest()
@@ -25,45 +27,67 @@ namespace BitMapTest
 
         private void Bitmaptest_Load(object sender, EventArgs e)
         {
-            int x = 0; int y = 0;
-
+           // int x = 0; int y = 0;
+           // string bob = null;
             //read image
             // Bitmap myBitmap = new Bitmap("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png");
             Bitmap myBitmap = getBitmap();  
             ///Get Data
-            //Color[,]
-                pixelData = new Color[myBitmap.Width, myBitmap.Height];
+            //make pixel data new colour array of the size of bitmapped image height and width
+           //     pixelData = new Color[myBitmap.Width, myBitmap.Height];
 
-            for (y = 0; y < myBitmap.Height; y++)
-                for (x = 0; x < myBitmap.Width; x++)
-                    pixelData[x, y] = myBitmap.GetPixel(x, y);
+            //for each y value 
+            //for (y = 0; y < myBitmap.Height; y++)
+            //{
+                //for each x value
+               // for (x = 0; x < myBitmap.Width; x++)
+                //{
+                  //  pixelPoint.Add(new Point (x,y));
+              //      pixelData[x, y] = myBitmap.GetPixel(x, y);
+
+                    //testing methods
+               //     bob = myBitmap.GetPixel(x, y).ToString();
+                 //   MessageBox.Show(bob);
+                //}
+         //   }
            
+
             //loadimage onto Screen
             pictureBox1.Image = myBitmap;
+
+            // add event handle to enable click
+            this.pictureBox1.MouseDown += new MouseEventHandler(this.pictureBox1_MouseDown);
         }
+      //  private void pict
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
             Point P = new Point(e.X,e.Y);
+          
 
-            for (int i = 0; i < pixelData.Length; i++) {
-                if (pixelData.GetValue(i).Equals(P))
-                {
+            //for (int i = 0; i < pixelData.Length ; i++)
+            //{
+               // for (int j = 0; j < pixelData.Length; i++)
+              //  {
+               //     string bob = pixelData.GetValue(i).ToString(); 
+                  //  pixelData.GetValue
+                    //bob = pixelData.ToString
+                   // MessageBox.Show(bob);
+                       Bitmap myBitmap = getBitmap();
+                      myBitmap.SetPixel(e.X, e.Y, Color.Red);
+              pixelPoint.Add(new Point (e.X,e.Y));
 
-                    Bitmap myBitmap = getBitmap();
-                    myBitmap.SetPixel(e.X, e.Y, Color.Red);
-                    pictureBox1.Image = myBitmap;
-                }
-               }
+            pictureBox1.Image = myBitmap;
+             //   }
+            //}
+         }
+            
+           //     else {
+             //       MessageBox.Show("Please try again.");
+               // }
+               
 
-        //    BitmapSource bSource = new BitmapImage(new Uri("H:\\MarkerlessSamples\\20160125_132341_00\\depth\\0.png"));
-           // BitmapSource img = pictureBox1.Image.get
-        //    int stride = bSource.PixelWidth * 4;
-       //     int size = bSource.PixelHeight * stride;
-       //     byte[] pixels = new byte[size];
-        //    bSource.CopyPixels(pixels, stride, 0);
-
-        }
+        //}
 
         public Bitmap getBitmap()
         {
